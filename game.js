92127a96756569541ea6cd05f9b2ddf9c6cb4d38
Yass-gr <grairiiyassin@gmail.com>
2025-12-13 23:56:39 +0000

    add dynamic ui and css
    added interactive ui with dom manipulation
    add styling with css
    instead of user entring his move in input now he can jusy click whatever move he wants
    instead of showing scores in an alert now scores show in the page
    added reset button to reset scores
    show what move each player chose

--- game.js
+++ game.js
@@ -2,12 +2,13 @@
 
 
 /* score declarations */
-let humanScore = 0;
-let computerScore = 0;
+  let humanScore = 0;
+  let computerScore = 0;
 
 
 
-/* getting computer choice*/
+
+/*computer's choice*/
 function getComputerChoice(){
     let randomNum = Math.random();
     if (randomNum < 0.333333333){
@@ -21,24 +22,9 @@ function getComputerChoice(){
 
 
 
-/* getting human choice*/
-function getHumanChoice(){
-    let choice = prompt("Enter your choice (paper,rock,scissors) :").toLowerCase();
-    if (choice === "paper"){
-        return "Paper";
-    } else if (choice === "scissors"){
-        return "Scissors";
-    }else if (choice === "rock"){
-        return "Rock";
-    }
-
-      }
-
-
-      
-/* playing a single round*/      
+/* playing a single round logic*/      
 function playRound(computerChoice,humanChoice) {
-  
+   
     if (computerChoice === humanChoice) {
         return `tie! you both chose ${computerChoice}`
     }else if (computerChoice === "Rock" && humanChoice === "Paper") { 
@@ -60,27 +46,61 @@ function playRound(computerChoice,humanChoice) {
         computerScore += 1
         return `you lose! ${computerChoice} beats ${humanChoice}`
     }
-    
-
-
-    
-}
-
-/* playing the whole game (5 rounds) and declaring the winner*/ 
-function playGame(){
-    for (i=0 ;i<5;i++) {
-        alert(`game ${i+1}:\n ` + playRound(getComputerChoice(),getHumanChoice()) + `\n score: computer:${computerScore} human:${humanScore}`)
-        
-    }
-    if (computerScore === humanScore) {
-        alert(`final score:${computerScore}:${humanScore} game ends in a tie!`)
-    }else if (computerScore > humanScore) {
-        alert(`final score:${computerScore}:${humanScore} computer wins!`)
-    }else if (computerScore < humanScore) {
-        alert(`final score:${computerScore}:${humanScore} human wins!`)
-    }
+       
 }
 
 
-/* trigger the game*/ 
-playGame()
+
+ //displaying the buttons
+    const humanChoices = document.querySelector(".human-choices")
+    const rock = document.createElement("button")
+    rock.textContent = "Rock"
+    const paper = document.createElement("button")
+    paper.textContent = "Paper"
+    const scissors = document.createElement("button")
+    scissors.textContent = "Scissors"
+    humanChoices.appendChild(rock)
+    humanChoices.appendChild(scissors)
+    humanChoices.appendChild(paper)
+
+//event listner for when user choses a move to trigger the round
+    let hMove = ""
+    humanChoices.addEventListener("click", function(e){
+        hMove = e.target.textContent
+        let gameResult = playRound(getComputerChoice(),hMove)
+        display = document.querySelector(".current-game")
+        display.textContent = gameResult
+        let scoreDisplayH = document.querySelector(".spn1")
+        let scoreDisplayC = document.querySelector(".spn2")
+        scoreDisplayH.textContent =  humanScore
+        scoreDisplayC.textContent = computerScore
+        let aiMove = getComputerChoice()
+        let moveH = document.querySelector(".spn3")
+        let moveC = document.querySelector(".spn4")
+        moveH.textContent = hMove
+        moveC.textContent = aiMove
+    })
+//event listner for when user presses restet button
+     const reset = document.querySelector("#reset")
+     reset.addEventListener("click", function() {
+     humanScore = 0;
+     computerScore = 0;
+     let scoreDisplayH = document.querySelector(".spn1")
+    let scoreDisplayC = document.querySelector(".spn2")
+    scoreDisplayH.textContent =  humanScore
+    scoreDisplayC.textContent =  computerScore
+    })
+
+    
+    
+    
+  
+
+
+  
+      
+
+
+
+ 
+
